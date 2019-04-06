4f19859a0ae2c27cf971f6d53bf31205f020fa99
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-04-06 17:59:29 +0200

    Ajout des données de base de la table CATEGORIE

--- resources/sql/insert.sql
+++ resources/sql/insert.sql
@@ -2,3 +2,6 @@
 -- insert.sql
 -- Ce script insère les données de base du site dans la bdd
 
+INSERT INTO CATEGORIE VALUES(0, "YouTube");
+INSERT INTO CATEGORIE VALUES(1, "Programmation");
+INSERT INTO CATEGORIE VALUES(2, "Autres");
