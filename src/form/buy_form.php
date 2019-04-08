e68618523f420cf1f243e83722032b3f5c3a9971
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-04-08 18:17:10 +0200

    Améliorations en tout genre, ajout d'une dépendance à html2pdf

--- src/form/buy_form.php
+++ src/form/buy_form.php
@@ -1,24 +1,40 @@
 <?php
     include_once("../php/bdd.php");
-    include_once("../php/comm_util.php");
+    include_once("../php/bill.php");
     session_start();
 
+    function getNextCommId($bdd){
+        $req = $bdd->query("SELECT COUNT(CodeComm) AS r FROM COMMANDE");
+        $codecomm = $req->fetch(PDO::FETCH_ASSOC);
+        return $codecomm["r"] + 1;
+    }
+    
     if (isset($_SESSION["id"])){
         if (count($_SESSION["products"]) !== 0){
-            $codecomm = getNextCommId($bdd);
-            $req = $bdd->prepare("INSERT INTO COMMANDE VALUES(?, ?, ?, ?)");
-            $req->execute(array($codecomm, gmdate('Y-m-d'), $_SESSION["total"], $_SESSION["id"]));
+            $codeComm = getNextCommId($bdd);
+            $req = $bdd->prepare("INSERT INTO COMMANDE(DateComm, PrixTotal, IdCli) VALUES(?, ?, ?)");
+            $req->execute(array(gmdate('Y-m-d'), $_SESSION["total"], $_SESSION["id"]));
             foreach($_SESSION["products"] as $prod => $qte){
                 $req = $bdd->prepare("INSERT INTO ACHETER VALUES(?, ?, ?)");
-                $req->execute(array($prod, $codecomm, $qte));
+                $req->execute(array($prod, $codeComm, $qte));
             }
             $_SESSION["products"] = array();
-            echo "La commande N°" . $codecomm . " a bien été enregistrée!";
-            // TODO 
+            ?>
+            <head>
+                <link rel="stylesheet" type="text/css" href="/style/common.css">
+                <meta charset="utf-8">
+                <script src="/scripts/html2pdf.bundle.min.js"></script>
+            </head>
+            <?php
+            echo "<p>La commande N°" . $codeComm . " a bien été enregistrée!</p>";
+            printBill($bdd, $codeComm, $_SESSION["id"]);
+            ?>
+                <button onclick="html2pdf(document.getElementById('bill'))">Télécharger le pdf</button>
+            <?php
         } else {
             ?>
                 <p>Votre panier est vide!</p>
-                <a href="/products">Ajouter des produits au panier</p>
+                <a href="/products">Ajouter des produits au panier</a>
                 <script>setTimeout(`location.href = '/products'`,4000)</script>
             <?php
         }
--- src/php/comm_util.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function getNextCommId($bdd){
-        $req = $bdd->query("SELECT COUNT(CodeComm) FROM COMMANDE");
-        $codecomm = $req->fetch(PDO::FETCH_BOUND);
-        $codecomm = $codecomm[0] + 1;
-        return $codecomm;
-    }
-?>
